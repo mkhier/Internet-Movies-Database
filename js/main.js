5a4056656f0c6cca85a3b42be23135102d7a1068
mkhier <mohannad.khier@gmail.com>
2022-08-04 18:47:03 +0300

    modifying Bootstrap and adding JS

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function addWatchlist(){    
+    document.getElementById('watchBtn').innerHTML= "<i class='fas fa-check'></i> Watchlisted </button>";    
+}
+function resetbtn(){
+    document.getElementById("loginForm").reset();
+}
